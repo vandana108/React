4f6d4e2f2ee30138f91290ad99730be28216ffea
vandana108 <thevandanarai108@gmail.com>
2025-03-04 03:02:07 +0530

    Used props

--- react-project-two/src/App.jsx
+++ react-project-two/src/App.jsx
@@ -3,18 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserCard from "./component/UserCard"
-
+import vandana from "./assets/vandana.jpg"
+import gautam from "./assets/gautam.jpg"
+import sunita from "./assets/sunita.jpg"
 function App() {
-  // const [count, setCount] = useState(0)
+
 
   return (
-    <div>
-      <div className='container'>
-        <UserCard/>
-        <UserCard/>
-        <UserCard/>
-      </div>
+    /*we can user here only one div*/
+    <div className="container"> 
+      <UserCard name = "Sunita" description="desc1" image={sunita} style={{"border-radius":"10px"}}/>
+      <UserCard name = "Vandana" description="desc2" image={vandana} style={{"border-radius":"10px"}}/>
+      <UserCard name = "Gautam" description="desc3" image={gautam} style={{"border-radius":"10px"}}/>
     </div>
+ 
+    // <div>
+    //   <div className='container'>//we can user here only one div
+    //     <UserCard/>
+    //     <UserCard/>
+    //     <UserCard/>
+    //   </div>
+    // </div>
     
   )
 }
--- react-project-two/src/component/UserCard.jsx
+++ react-project-two/src/component/UserCard.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import vandana from '../assets/vandana.jpg'
+
 import "./UserCard.css"
-const UserCard = ( )=> {
+const UserCard = (props)=> {//for multiple username
   return(
-    <div className="user-container">
-      <p id="title">Vanadanatm</p>
-      <img id='user-img' src={vandana} alt="vandana"></img>
-      <p id="user-description">Description of Profile</p>
+    // <div className="user-container">
+    //   <p id="title">Vanadanatm</p>
+    //   <img id='user-img' src={vandana} alt="vandana"></img>
+    //   <p id="user-description">Description of Profile</p>
 
 
-    </div>
+    // </div>
+
+    <div className="user-container" style={props.style}>
+      <p id='user-name'>{props.name}</p>
+      <img id='user-img' src={props.image} alt='{props.name}'></img>
+      <p id='user-desc'>{props.description}</p>
+   </div>
     
   )
   }
