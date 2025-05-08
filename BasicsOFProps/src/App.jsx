f9a829aa4ce33277a03ce0d826578caee5df8e80
vandana108 <thevandanarai108@gmail.com>
2025-05-08 11:05:23 +0530

    Initial commit

---
+++ BasicsOFProps/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+   <div>
+   <Card name="love babbar">
+    <h1>Best web development course</h1>
+    <p>Trying to be consistent in this course</p>
+    <p>well, see when I win hackathon</p>
+   </Card>
+   <Card>
+    <h1>Hello Team, Hope yor're doing great!!</h1>
+   </Card>
+   </div>
+  )
+}
+
+export default App
