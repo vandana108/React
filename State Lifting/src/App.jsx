fa66d4d1bd9a01e6a1a2116e2051a25ffd812aa2
vandana108 <thevandanarai108@gmail.com>
2025-05-09 15:42:21 +0530

    State Lifting

---
+++ State Lifting/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Card name ="Love Babbar" />
+    </div>
+  )
+}
+
+export default App
